91870a223ef5b34cec0141828630fd426a3fe950
Khoa-Cifer <ulquiorra10203@gmail.com>
2024-05-12 20:11:49 +0700

    stable version

--- backend/src/main/java/com/cifer/lakeSidehotel/exception/InvalidBookingRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.cifer.lakeSidehotel.exception;
-
-public class InvalidBookingRequestException extends RuntimeException{
-    public InvalidBookingRequestException(String message) {
-        super(message);
-    }
-}
--- backend/src/main/java/com/cifer/lakeSidehotel/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.cifer.lakeSidehotel.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
